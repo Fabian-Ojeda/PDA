9210564e8c9fb34f67af644484f6df58154bca5d
johan-martinez <johan.martinez01@uptc.edu.co>
2021-10-05 23:08:49 -0500

    Separando en paquetes

--- src/view/IoManager.java
+++ src/view/IoManager.java
@@ -1,30 +1,58 @@
 package view;
 
+import model.PdaAutomatum;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class IoManager {
+public class IoManager extends JFrame{
 
-    public IoManager(){
+    private PanelForm panelForm;
+    private PanelPda panelPda;
 
+    public IoManager(ActionListener l){
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        this.setSize(500,700);
+        this.setLocationRelativeTo(null);
+        initComponents(l);
+        this.setTitle("PDA");
+        this.setVisible(true);
     }
 
-    public String readString(String message){
-        String toReturn = JOptionPane.showInputDialog(null, message);
-        return toReturn;
+    private void initComponents(ActionListener l) {
+        this.panelForm=new PanelForm(l);
+        this.panelPda=new PanelPda(l);
+        this.add(panelForm, BorderLayout.CENTER);
     }
 
-    public ArrayList<String> readStrings(String message){
-        ArrayList<String> temp=new ArrayList<>();
-        int val=1;
-        do {
-            temp.add(readString(message));
-            val=Integer.parseInt(readString("DESEA INGRESAR MÁS CADENAS: \n0)no 1) SI "));
-        } while(val==1);
-        return temp;
+    public PdaAutomatum getAutomatum(){
+        return panelForm.createPda();
     }
 
     public void showInfo(String message){
         JOptionPane.showMessageDialog(null, message);
+    }
+
+    public void showPda(String info) {
+        this.panelPda.changeInfo(info);
+        this.panelForm.setVisible(false);
+        this.add(panelPda, BorderLayout.CENTER);
+
+        panelPda.setVisible(true);
+    }
+
+    public String getWord(){
+        return panelPda.getWord();
+    }
+    public void showForm() {
+        this.panelPda.changeInfo("");
+        panelPda.clean();
+        this.panelPda.setVisible(false);
+        this.add(panelForm, BorderLayout.CENTER);
+
+        panelForm.setVisible(true);
     }
 }
